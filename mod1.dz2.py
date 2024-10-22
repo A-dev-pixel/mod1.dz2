2ea15155573753c6928464a7d68372530d9021a9
A-dev-pixel <podlesnovav91@gmail.com>
2024-10-22 19:35:32 +0300

    Add files via upload

---
+++ mod1.dz2.py
@@ -0,0 +1,13 @@
+Six_name= ("Количество выполненных ДЗ:")
+twelve = 12
+seven_name= ("затраченно часов:")
+Five = 1.5
+first_name  = ("Название курса:")
+second_name = ("Python,")
+Time_name= ("среднее время выполнения:")
+str= 12-1.5
+third_name = ("всего задач")
+hundred_name = Five/twelve
+print (hundred_name)
+hour_name=("часа")
+print(second_name , third_name ,twelve,  seven_name, Five, Time_name, hundred_name, hour_name)
